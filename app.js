b070a5e8a55f898166b573795865a5f7c613aec1
megimesser <166508972+megimesser@users.noreply.github.com>
2024-06-06 15:47:39 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+let kommentare = document.getElementById("kommentare")
+let name = document.getElementsById("name")
+let kommentar = document.getElementsById("kommentar")
+let button = document.getElementsById("button")
+
+button.addEventListener("click", function() {
+    console.log("hi")
+})
